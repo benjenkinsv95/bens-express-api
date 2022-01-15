d898ba3512935fb8ea19efc1d23e44d4136b2669
benjenkinsv95 <benjenkinsv95@gmail.com>
2022-01-15 13:36:45 -0600

    Add practice and skill models

---
+++ app/models/practice.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const moment = require('moment')
+
+const practiceSchema = new mongoose.Schema(
+  {
+    streakStart: Date,
+    lastPracticed: Date,
+    skill: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Skill',
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+practiceSchema.virtual('streakStart').get(function () {
+  // if we've never practiced there isnt a streak
+  if (!this.lastPracticed || !this.streakStart) {
+    return 0
+  }
+
+  // convert dates into momentjs
+  const todayMoment = moment()
+  const yesterdayMoment = moment().subtract(1, 'day')
+  const lastPracticedMoment = moment(this.lastPracticed)
+  const streakStartMoment = moment(this.streakStart)
+
+  const isToday = lastPracticedMoment.isSame(todayMoment, 'day')
+  const isYesterday = lastPracticedMoment.isSame(yesterdayMoment, 'day')
+
+  // if the we haven't practiced today or yesterday, there isn't a streak
+  if (!isToday && !isYesterday) {
+    return 0
+  }
+
+  // add 1 for the day the streak started
+  return 1 + lastPracticedMoment.diff(streakStartMoment, 'days')
+})
+
+module.exports = mongoose.model('Practice', practiceSchema)
